1074a4ff70ac1739b05a186b6f760c0bd238594f
Venkat <vke.1743@gmail.com>
2025-01-10 21:30:17 +0530

    index.html and add_course.html

--- app.py
+++ app.py
@@ -46,6 +46,39 @@ def course_details(code):
     return render_template('course_details.html', course=course)
 
 
+@app.route('/add_course', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'POST':
+        course = {
+            'code': request.form['code'],
+            'name': request.form['name'],
+            'instructor': request.form['instructor'],
+            'semester': request.form['semester'],
+            'schedule': request.form['schedule'],
+            'classroom': request.form['classroom'],
+            'prerequisites': request.form['prerequisites'],
+            'grading': request.form['grading'],
+            'description': request.form['description']
+        }
+        missing = []
+        if course["code"] == "" : missing.append('code')
+        if course["name"] == "": missing.append('name')
+        if course["instructor"] == "" : missing.append('instructor') 
+        if(len(missing) != 0):
+            flash(f'''
+                    Please fill in all the required fields,
+                    missing fields: {missing}
+                ''', "error")
+            # logging.error(f"Course details are missing.  Please try again by filling in all the required fields: Course code, Course name and Instructor. Missing fields: {missing}")
+            return render_template('add_courses.html', course=course)
+        
+        else:
+            save_courses(course)    
+            flash(f"Course '{course['name']}' added successfully!", "success")
+            # logging.info(f"Course '{course['name']}' with code '{course['code']}' added to the catalog.")
+            return redirect(url_for('course_catalog'))
+    return render_template('add_courses.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
